23a2d066c434d9c55a6e071072510c3a9547103b
Fredrik Baberg <fbaberg@kth.se>
2017-08-29 18:18:22 +0200

    Enough to copy to ~/bin.

--- docker_latex/dockerlatex.sh
+++ docker_latex/dockerlatex.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Using: Run this script with a .tex file as argument, e.g. dockerlatex.sh example.tex, to compile the document 'example.tex'.
-# Hint: Copy this to /usr/local/bin/dockerlatex, and chmod +x, to be able to call the script from anywhere (as dockerlatex <filename.tex>).
+# Hint: Copy this to ~/bin/ and chmod +x to be able to call it from anywhere (as dockerlatex.sh <filename.tex>).
 echo "This script will now:"
 echo "* Create folders 'build' and 'pdf',"
 echo "* Run pdflatex once for $1 (run again to fix references etc.),"
