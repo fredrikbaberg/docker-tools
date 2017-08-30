c42aa1a51aa6436a0150771902d41feac342efb0
Fredrik Baberg <fbaberg@kth.se>
2017-08-30 14:42:33 +0200

    dockerlatex.sh now runs also runs bibtex.

--- docker_latex/dockerlatex.sh
+++ docker_latex/dockerlatex.sh
@@ -3,7 +3,11 @@
 # Hint: Copy this to ~/bin/ and chmod +x to be able to call it from anywhere (as dockerlatex.sh <filename.tex>).
 echo "This script will now:"
 echo "* Create folders 'build' and 'pdf',"
-echo "* Run pdflatex once for $1 (run again to fix references etc.),"
+echo "* Run pdflatex once for $1,"
+echo "* Run bibtex once for $1,"
+echo "* Run pdflatex twice to update references,"
 echo "* Copy any generated PDF files into 'pdf' folder."
 echo ""
-docker run --rm -i --user="$(id -u):$(id -g)" -v $PWD:/docs docker_latex mkdir --p build pdf && pdflatex -output-directory build "$@" && cp $PWD/build/*.pdf $PWD/pdf/
+
+# docker run --rm -i --user="$(id -u):$(id -g)" -v $PWD:/docs docker_latex mkdir --p build pdf && pdflatex -output-directory build "$@" && cp $PWD/build/*.pdf $PWD/pdf/
+docker run --rm -i --user="$(id -u):$(id -g)" -v $PWD:/docs docker_latex mkdir --p build pdf && pdflatex --output-directory build "$@" && TEXMFOUTPUT=build/ bibtex build/$(echo $1 | sed 's=.*/==;s/\.[^.]*$//') && pdflatex -output-directory build "$@" && pdflatex --output-directory build "$@" && cp $PWD/build/*.pdf $PWD/pdf/
