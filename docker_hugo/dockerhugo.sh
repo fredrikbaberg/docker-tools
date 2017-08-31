729db7aa4955efffe9db620fdb86fc217e87a010
Fredrik Baberg <fbaberg@kth.se>
2017-08-31 16:58:16 +0200

    Migh need more debugging.

--- docker_hugo/dockerhugo.sh
+++ docker_hugo/dockerhugo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Runs the "hugo" command, with whatever arguments are passed.
-docker run --rm -i --user="$(id -u):$(id -g)" -v $PWD:/site docker_hugo hugo "$@"
+# For "server", make sure to add --bind "0.0.0.0".
+docker run --rm -it --user="$(id -u):$(id -g)" -p 1313:1313 -v $PWD:/site docker_hugo hugo "$@"
